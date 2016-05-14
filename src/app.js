e5b4f0fc82997949ab80c7f466218bb6e0ecdbd9
Francois <francoisthoraval@hotmail.fr>
2016-05-14 12:24:43 +0200

    Git de ses morts

--- src/app.js
+++ src/app.js
@@ -2,8 +2,14 @@ var express = require('express');
 var ejs = require('ejs');
 var app = express();
 
+
 app.get('/', function(req, res){
   res.render('RoyalPac.ejs');
+});
+
+app.use(function(req, res, next){
+  res.setHeader('Content-Type', 'text/plain');
+  res.send(404, 'ERREUR 404 MEC ! (Coded by Eddy Murhpy ...)!');
 });
 
 app.listen(8080);
