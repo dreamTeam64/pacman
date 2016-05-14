797d59106167e8fafa59991c8b25f211dbc16f35
Francois <francoisthoraval@hotmail.fr>
2016-05-14 17:29:58 +0200

    La map apparait bordel de cul !

--- src/public/script/RoyalPac.js
+++ src/public/script/RoyalPac.js
@@ -2,15 +2,18 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create
 
 function preload() {
     game.load.tilemap('ClassicMap', '../assets/RoyalPac-mapV2.json', null, Phaser.Tilemap.TILED_JSON);
-
+    game.load.image('tiles', '../assets/TileSet.png');
 }
 
 var map;
+var layer;
 
 function create() {
 
     map = game.add.tilemap('ClassicMap');
-
+    map.addTilesetImage('TileSet', 'tiles');
+    layer = map.createLayer('Calque de Tile 1');
+    layer.resizeWorld();
 }
 
 function update() {
