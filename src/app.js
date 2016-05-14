c8f04bcdbd0d0aa1f5130e1eb9e014620a18a2ca
fontaine pierre <pierre.ftn64@gmail.com>
2016-05-14 13:45:36 +0200

    route vers les scripts

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var ejs = require('ejs');
 var app = express();
 
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
   res.render('RoyalPac.ejs');
@@ -11,5 +12,7 @@ app.use(function(req, res, next){
   res.setHeader('Content-Type', 'text/plain');
   res.send(404, 'ERREUR 404 MEC ! (Coded by Eddy Murhpy ...)!');
 });
+
+
 
 app.listen(8080);
