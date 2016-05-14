d3475c27ea00033f1cd89a192f0fbd1ba8fa99ad
fontaine pierre <pierre.ftn64@gmail.com>
2016-05-14 12:07:21 +0200

    assets/ moved into src/ & RoyalPac added as main page

--- src/app.js
+++ src/app.js
@@ -2,8 +2,8 @@ var express = require('express');
 var ejs = require('ejs');
 var app = express();
 
-app.get('/:prenom', function(req, res){
-  res.render('accueil.ejs',{name: req.params.prenom});
+app.get('/', function(req, res){
+  res.render('RoyalPac.ejs');
 });
 
 app.listen(8080);
