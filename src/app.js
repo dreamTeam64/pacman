5a51ea898bd11e912675b01d76f89919440e3ee0
fontaine pierre <pierre.ftn64@gmail.com>
2016-05-14 14:57:54 +0200

    Recherche solution pour charger tout les scripts

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 var express = require('express');
+var server = require('http').createServer(app);
 var ejs = require('ejs');
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
-
+app.set('view engine', 'ejs');
 app.get('/', function(req, res){
+  res.sendFile(__dirname + '/public/assets/RoyalPac-mapV2.json');
   res.render('RoyalPac.ejs');
 });
 
@@ -12,7 +14,5 @@ app.use(function(req, res, next){
   res.setHeader('Content-Type', 'text/plain');
   res.send(404, 'ERREUR 404 MEC ! (Coded by Eddy Murhpy ...)!');
 });
-
-
 
 app.listen(8080);
--- src/views/script/RoyalPac.js
+++ src/views/script/RoyalPac.js
@@ -1,8 +1,7 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 function preload() {
-    game.load.tilemap('ClassicMap', '../../assets/RoyalPac-mapV2.json', null, Phaser.Tilemap.TILED_JSON);
-
+    game.load.tilemap('ClassicMap', '/assets/RoyalPac-mapV2.json', null, Phaser.Tilemap.TILED_JSON);
 }
 
 var map;
