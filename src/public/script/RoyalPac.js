ac26f27b4587d10b96268254335038e7e3747358
fontaine pierre <pierre.ftn64@gmail.com>
2016-05-14 18:29:16 +0200

    spritesheet added and loaded into phaser

--- src/public/script/RoyalPac.js
+++ src/public/script/RoyalPac.js
@@ -3,10 +3,12 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create
 function preload() {
     game.load.tilemap('ClassicMap', '../assets/RoyalPac-mapV2.json', null, Phaser.Tilemap.TILED_JSON);
     game.load.image('tiles', '../assets/TileSet.png');
+    game.load.spritesheet('pacman','../assets/pacman.png',35,35,5);
 }
 
 var map;
 var layer;
+var player;
 
 function create() {
 
@@ -14,6 +16,7 @@ function create() {
     map.addTilesetImage('TileSet', 'tiles');
     layer = map.createLayer('Calque de Tile 1');
     layer.resizeWorld();
+    player = game.add.sprite(35,game.world.height - 100,'pacman');
 }
 
 function update() {
