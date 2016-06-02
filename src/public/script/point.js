a890e49b52a20e73cbd40cdade852aa69ec488be
fontaine pierre <pierre.ftn64@gmail.com>
2016-06-02 15:30:29 +0200

    point to object

--- src/public/script/point.js
+++ src/public/script/point.js
@@ -1,36 +1,60 @@
-point = function(game,layer,type,value,player,score){
-    //this.hit = false;
-    this.type = type; // Simple ou Fruit
-    this.value = value; // 1, ou 50 pour le fruit
+point = function(game,layer){
+  this.game = game;
+  this.layer = layer;
 
-    this.game = game;
-    this.layer = layer;
-    this.player = player;
+  this.score = score;
+  this.scoreText = null;
 
-    this.score = score;
+  points.game.add.group();
 
-    points.game.add.group();
-}
-point.prototype = Object.create(Phaser.Sprite.prototype);
-point.prototype.constructor = point;
+  this.PlacePoint = function(){
 
-point.prototype.getScore = function(){return this.score}
+    superPoint = game.add.group();
+    superPoint.enableBody = true;
 
-/*
-point.prototype.create = function () {
+    points = game.add.group();
+    points.enableBody = true;
+
     var i=0;
     var j=0;
+    var GrosPoint;
+
+    do {
+        GrosPoint = Math.floor((Math.random() * 25));
+    } while ((map.getTile(GrosPoint,GrosPoint,layer,true).index!=136)||(map.getTile(GrosPoint,GrosPoint,layer,true).index==1)||(map.getTile(GrosPoint,GrosPoint,layer,true).index==69));
+
     for (i = 0; i < 31; i++) {
-        for (j = 0; j < 24; j++) {
-            if (map.getTile(i,j,this.layer,true).index == 136) {
-                var Point = this.points.create(i*25,j*25,'star');
-            }
-        }
+      for (j = 0; j < 24; j++) {
+          if ((GrosPoint==i*25)&&(GrosPoint==j*25)) { // Placer le super point
+              superPoint.create(i*25,j*25,'diamond');
+              j++;
+          }
+          if ((i*25==player.position.x) && (j*25==player.position.y)) { //Pas d'étoiles sur le pacman
+              j++;
+          }
+          if (map.getTile(i,j,layer,true).index == 136) {
+                  var point = points.create(i*25,j*25,'LittlePoint');
+                  this.howLeft += 10;
+          }
+      }
     }
-};
-*/
 
+    this.howLeft -= 1;
+  }
 
-point.prototype.update = function(){
-    //this.game.physics.arcade.overlap(this.player,this.points,Scoring,null,this);
+  this.reset = function(){
+    if (this.howLeft == 0) {
+        this.PlacePoint();
+        level++;
+        levelText.text = 'current level: '+ level;
+    }
+  }
+
+  this.scoring = function(){
+    this.Point.kill();
+    this.score += 10;
+    scoreText.text = 'score: '+ score;
+    this.howLeft -= 10;
+  }
+
 }
