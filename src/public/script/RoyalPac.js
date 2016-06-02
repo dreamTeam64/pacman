b82a4281b7f5d38f895a14ed57f1d8e19b20d59e
ThePanda33 <francoisthoraval@hotmail.fr>
2016-06-02 12:16:38 +0200

    plus de point sur le pacman au demarrage et plus detoiles, maintenant cest de vrais points

--- src/public/script/RoyalPac.js
+++ src/public/script/RoyalPac.js
@@ -7,11 +7,12 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create
 function preload() {
     game.load.tilemap('ClassicMap', '../assets/RoyalPac-mapV2.json', null, Phaser.Tilemap.TILED_JSON);
     game.load.image('tiles', '../assets/TileSet.png');
-    game.load.image('star', '../assets/star.png');
+    game.load.image('LittlePoint', '../assets/LittlePoint.png');
     game.load.spritesheet('pacman','../assets/pacman_test.png',25,25,13);
     game.load.spritesheet('greendy','../assets/greendy.png',25,25,4);
     game.load.spritesheet('reddit','../assets/reddit.png',25,25,4);
     game.load.spritesheet('yellowStone','../assets/yellowStone.png',25,25,4);
+    game.load.spritesheet('diamond','../assets/diamond.png',25,25,4);
 }
 
 /* Déclaration des variables globales au jeu */
@@ -35,6 +36,9 @@ var blocked = false;
 var pathfinder;
 var walkables;
 
+var superPoint;
+var superPointValue=50;
+
 function create() {
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
@@ -56,6 +60,8 @@ function create() {
 
     game.physics.arcade.collide(player, layer);
     player = game.add.sprite(375,375,'pacman');
+
+    // superPoint = game.add.sprite(375,375,'diamond');
 
     game.physics.enable(player);
     player.body.collideWorldBounds = true;
@@ -159,20 +165,28 @@ function isStick(player,layer,map){
 function PlacePoint(){
     //INSTANCE DES POINTS
 
-
+    superPoint = game.add.group();
+    superPoint.enableBody = true;
     points = game.add.group();
     points.enableBody = true;
     var i=0;
     var j=0;
+    var GrosPoint;
+    do {
+        GrosPoint = Math.floor((Math.random() * 25));
+    }while ((map.getTile(GrosPoint,GrosPoint,layer,true).index!=136)||(map.getTile(GrosPoint,GrosPoint,layer,true).index==1)||(map.getTile(GrosPoint,GrosPoint,layer,true).index==69));
+
     for (i = 0; i < 31; i++) {
             for (j = 0; j < 24; j++) {
-                console.log("player.position.x = "+player.position.x);
-                console.log("player.position.y = "+player.position.y);
-                if ((i*25==player.position.x) && (j*25==player.position.y)) {
-                    i++;
+                if ((GrosPoint==i*25)&&(GrosPoint==j*25)) { // Placer le super point
+                    superPoint.create(i*25,j*25,'diamond');
+                    j++;
+                }
+                if ((i*25==player.position.x) && (j*25==player.position.y)) { //Pas d'étoiles sur le pacman
+                    j++;
                 }
                 if (map.getTile(i,j,layer,true).index == 136) {
-                        var point = points.create(i*25,j*25,'star');
+                        var point = points.create(i*25,j*25,'LittlePoint');
                         howLeft = howLeft+10;
                 }
             }
