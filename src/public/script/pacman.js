55b191398b920d0a70acf10a575d24cfa4372fa6
fontaine pierre <pierre.ftn64@gmail.com>
2016-06-05 22:17:57 +0200

    avancement

--- src/public/script/pacman.js
+++ src/public/script/pacman.js
@@ -11,14 +11,14 @@ var pacman = function(game,layer,x,y){
 pacman.prototype = Object.create(Phaser.Sprite.prototype);
 pacman.prototype.constructor = pacman;
 
-pacman.prototype.moveUp = function(){
+pacman.prototype.canGo = function() {
 
 }
 
-pacman.prototype.moveDown = function(){
+pacman.prototype.moveUp = function(){
   this.body.velocity.x = -this.velocityPlayer;
   //  Move to the left
-  if (canGo('left',player,layer,map)){
+  if (this.canGo('left',this,this.layer,this.map)){
     this.body.velocity.x = -this.velocityPlayer;
     this.body.velocity.y = 0;
     this.animations.play('left');
@@ -26,10 +26,35 @@ pacman.prototype.moveDown = function(){
   }
 }
 
-pacman.prototype.moveRight = function(){
+pacman.prototype.moveDown = function(){
+  this.body.velocity.y = this.velocityPlayer;
+  //  Move down
+  if (this.canGo('down',this,this.layer,this.map)){
+    this.body.velocity.y = this.velocityPlayer;
+    this.body.velocity.x = 0;
+    this.animations.play('down');
+    this.direction = 'down';
+  }
+}
 
+pacman.prototype.moveRight = function(){
+  this.body.velocity.x = this.velocityPlayer;
+  //  Move to the right
+  if (thiscanGo('right',this,this.layer,this.map)){
+    this.body.velocity.x = this.velocityPlayer;
+    this.body.velocity.y = 0;
+    this.animations.play('right');
+    this.direction = 'right';
+  }
 }
 
 pacman.prototype.moveLeft = function(){
-
+  this.body.velocity.x = -this.velocityPlayer;
+  //  Move to the left
+  if (thiscanGo('left',this,this.layer,this.map)){
+    this.body.velocity.x = -this.velocityPlayer;
+    this.body.velocity.y = 0;
+    this.animations.play('left');
+    this.direction = 'left';
+  }
 }
