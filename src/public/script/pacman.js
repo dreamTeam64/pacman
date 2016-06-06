5260025f5697418aa99769ed3b75bf4be4a4bfea
fontaine pierre <pierre.ftn64@gmail.com>
2016-06-06 11:32:46 +0200

    sdfsdf

--- src/public/script/pacman.js
+++ src/public/script/pacman.js
@@ -1,10 +1,22 @@
 var pacman = function(game,layer,x,y){
   this.game = game;
-  this.layer = layer,
+  this.layer = layer;
+
   this.x = x;
   this.y = y;
 
+  this.speed_x = 0;
+  this.speed_y = 0;
+
+  this.tile_x = null;
+  this.tile_y = null;
+
+  this.relativeSpeed = 0;
+
   this.velocityPlayer = 30;
+
+  game.add.existing(this);
+  game.physics.enable(this);
 
 }
 
@@ -15,46 +27,67 @@ pacman.prototype.canGo = function() {
 
 }
 
+pacman.prototype.isStick = function(){
+
+}
+
 pacman.prototype.moveUp = function(){
-  this.body.velocity.x = -this.velocityPlayer;
+  this.speed_x = -this.velocityPlayer;
   //  Move to the left
   if (this.canGo('left',this,this.layer,this.map)){
-    this.body.velocity.x = -this.velocityPlayer;
-    this.body.velocity.y = 0;
+    this.speed_x = -this.velocityPlayer;
+    this.speed_y = 0;
     this.animations.play('left');
     this.direction = 'left';
   }
 }
 
 pacman.prototype.moveDown = function(){
-  this.body.velocity.y = this.velocityPlayer;
+  this.speed_y = this.velocityPlayer;
   //  Move down
   if (this.canGo('down',this,this.layer,this.map)){
-    this.body.velocity.y = this.velocityPlayer;
-    this.body.velocity.x = 0;
+    this.speed_y = this.velocityPlayer;
+    this.speed_x = 0;
     this.animations.play('down');
     this.direction = 'down';
   }
 }
 
 pacman.prototype.moveRight = function(){
-  this.body.velocity.x = this.velocityPlayer;
+  this.speed_x = this.velocityPlayer;
   //  Move to the right
   if (thiscanGo('right',this,this.layer,this.map)){
-    this.body.velocity.x = this.velocityPlayer;
-    this.body.velocity.y = 0;
+    this.speed_x = this.velocityPlayer;
+    this.speed_y = 0;
     this.animations.play('right');
     this.direction = 'right';
   }
 }
 
 pacman.prototype.moveLeft = function(){
-  this.body.velocity.x = -this.velocityPlayer;
+  this.speed_x = -this.velocityPlayer;
   //  Move to the left
   if (thiscanGo('left',this,this.layer,this.map)){
-    this.body.velocity.x = -this.velocityPlayer;
-    this.body.velocity.y = 0;
+    this.speed_x = -this.velocityPlayer;
+    this.speed_y = 0;
     this.animations.play('left');
     this.direction = 'left';
   }
+}
+
+pacman.prototype.update = function(){
+  game.physics.arcade.collide(this, this.layer);
+  //Mouvement du fantome en x et y
+  this.body.velocity.x = this.speed_x;
+  this.body.velocity.y = this.speed_y;
+
+  //formule de notre ami Pyhthagore, pour une fois que tu sert à quelque chose !
+  this.relativeSpeed = Math.sqrt(Math.pow(Math.abs(this.body.x - this.x),2) + Math.pow(Math.abs(this.body.y - this.y),2));
+
+  //mise à jour des coordonnées de l'objet
+  this.y = this.body.y;
+  this.x = this.body.x;
+
+  this.tile_x = Math.floor(this.x/25);
+  this.tile_y = Math.floor(this.y/25);
 }
